ccb269833863ad8f1ee25ae68f8addc580bbd751
JulesBecquelin <jules.becquelin@edu.univ-fcomte.fr>
2024-12-13 13:35:02 +0100

    menu qui fonctionne

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuBtn = document.querySelector('.header__menu-btn');
-    const menu = document.getElementById('mainNav');
+const NomDuBouton = document.querySelector('.header__menu-btn');
+const NomDuMenu = document.querySelector('.header__menu');
 
-    menuBtn.addEventListener('click', function() {
-        const isOpen = menu.classList.toggle('menu--open');
-        menuBtn.setAttribute('aria-expanded', isOpen);
-    });
+NomDuBouton.addEventListener("click", () =>{
+    toggle(NomDuBouton, NomDuMenu)
 });
+
+function toggle (btn, menu)  {
+    console.log("Je fonctionne !")
+
+    const isOpen = btn.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+
+    menu.ariaHidden = isOpen;
+    btn.ariaExpanded = isClosed;
+}

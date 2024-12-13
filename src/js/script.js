24f3cf879403c5713eb7ee32e6c22293ee3c932b
JulesBecquelin <jules.becquelin@edu.univ-fcomte.fr>
2024-12-13 15:37:47 +0100

    ajout menu

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,28 @@ function toggle (btn, menu)  {
     menu.ariaHidden = isOpen;
     btn.ariaExpanded = isClosed;
 }
+
+
+
+
+const carousel = document.querySelector(".caroussel__item");
+const prevButton = document.querySelector(".carousel__button carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button carousel__button--next");
+const premierItem = document.querySelector(".image-carroussel__hero");
+
+if (carousel && prevButton && nextButton && premierItem) {
+    prevButton.addEventListener("click", () => {
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+
+    nextButton.addEventListener("click", () => {
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+}
+
+
+
+
+
